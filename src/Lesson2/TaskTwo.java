03c760a1a0685ccd9ce58998b5b9f6178b0455bf
NataliaOrlova <natakarelskaya@yandex.ru>
2022-11-30 22:14:01 +0400

    task 2

--- src/Lesson2/TaskTwo.java
+++ src/Lesson2/TaskTwo.java
@@ -1,4 +1,23 @@
 package Lesson2;
-
 public class TaskTwo {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int prevColor = image[sr][sc];
+        if (prevColor == color) {
+            return image;
+        }
+        dfs(image, sr, sc, prevColor, color);
+        return image;
+    }
+
+    public void dfs(int[][] image, int sr, int sc, int prevColor, int color) {
+        if (sr < 0 || sr >= image.length || sc < 0
+                || sc >= image[0].length || image[sr][sc] != prevColor) {
+            return;
+        }
+        image[sr][sc] = color;
+        dfs(image, sr - 1, sc, prevColor, color);
+        dfs(image, sr + 1, sc, prevColor, color);
+        dfs(image, sr, sc - 1, prevColor, color);
+        dfs(image, sr, sc + 1, prevColor, color);
+    }
 }
