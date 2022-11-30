fe8116fc3e758e9ef391e8de08c89488bd83fbc8
NataliaOrlova <natakarelskaya@yandex.ru>
2022-11-30 18:45:44 +0400

    task 4

--- src/Lesson2/TaskFour.java
+++ src/Lesson2/TaskFour.java
@@ -1,4 +1,32 @@
 package Lesson2;
 
+//Definition for a binary tree node.
+//public class TreeNode {
+//    int val;
+//    TreeNode left;
+//    TreeNode right;
+//    TreeNode() {}
+//    TreeNode(int val) { this.val = val; }
+//    TreeNode(int val, TreeNode left, TreeNode right) {
+//        this.val = val;
+//        this.left = left;
+//        this.right = right;
+//    }
+//}
 public class TaskFour {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q != null) {
+            return false;
+        }
+        if (p != null && q == null) {
+            return false;
+        }
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p.val != q.val) {
+            return false;
+        }
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
+    }
 }
